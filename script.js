24236e29a3367c69e7dbb9c659b8bad8c125a715
Samuel Alegre <samuelalecas@gmail.com>
2022-08-31 20:53:09 +0200

    Create buttons of New Table and Clear & Define basic UI

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+var tableSize = 16;
+
 function newTable(row, columns) {
     const container = document.getElementById("container");
     for (let i = 0; i < row; i++) {
@@ -6,18 +8,49 @@ function newTable(row, columns) {
         for (let i = 0; i < columns; i++) {
             const box = document.createElement("div");
             box.classList.add("box");
+            box.addEventListener('mouseover', function paint() {
+                box.classList.add("painted")
+            })
             row.appendChild(box);
         }
         container.appendChild(row);
     }
 }
+function clear() {
+    const boxes = document.querySelectorAll(".box");
+    for (let box of boxes) {
+        box.classList.remove("painted");
+    }
+}
 
+function clearFullTable() {
+    const cont = document.getElementById("container");
+    cont.textContent = "";
+}
+
+function defineTableSize() {
+    let sizeIsOK = false;
+    tableSize = prompt("Write the number of squares of the new canvas:")
+    while (sizeIsOK === false) {
+        if (tableSize === null) {
+            console.log("I'm a null...")
+            // clearFullTable();
+            // newTable(16, 16);
+            sizeIsOK = true;
+        } else if (tableSize > 100 || tableSize < 5) {
+            tableSize = prompt("Please, choose a number between 5 and 100:")
+        } else {
+            clearFullTable();
+            newTable(tableSize, tableSize);
+            sizeIsOK = true;
+        }
+    }
+}
 
 newTable(16, 16);
 
-const boxes = document.querySelectorAll(".box");
-for(let box of boxes){
-    box.addEventListener('click', function paint(){
-        box.classList.toggle("painted")
-    })
-}
+const clearButton = document.getElementById("clear");
+clearButton.addEventListener("click", clear)
+
+const newTableButton = document.getElementById("table");
+newTableButton.addEventListener("click", defineTableSize);
